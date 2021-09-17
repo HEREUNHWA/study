f04bf81a42e50a1b17d42bfc8c51e9ca8384a31e
HEREUNHWA <hsghm3824@naver.com>
2021-09-17 12:54:54 +0900

    ReviewDemo01 v0.5

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,4 +1,39 @@
 import java.util.Scanner;
+// v0.5
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        //소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean isPrime = true;
+
+//       for(int k=2; k<number; k++){ //반복 횟수 2회 감소
+ //           if(number % k ==0) {
+ //               isPrime = false;
+ //               break;  // 첫 번째 약수가 발견되면 for문 탈출
+ //           }
+ //           //System.out.println(k);
+ //       }
+
+        int k=2;
+        while (k<number){ //반복 횟수 2회 감소
+            if(number % k ==0) {
+                isPrime = false;
+                break;  // 첫 번째 약수가 발견되면 for문 탈출
+            }
+            //System.out.println(k);
+            k++;
+        }
+
+        if(isPrime) // isPrime 변수 값이 true면
+            System.out.println(number + "은(는) 소수!");
+        else
+            System.out.println(number + "은(는) 소수가 아닙니다");
+    }
+}
+
+/*
+import java.util.Scanner;
 // v0.3
 public class ReviewDemo01 {
     public static void main(String[] args) {
@@ -18,6 +53,7 @@ public class ReviewDemo01 {
             System.out.println(number + "은(는) 소수가 아닙니다");
     }
 }
+*/
 
 /*
 import java.util.Scanner;
